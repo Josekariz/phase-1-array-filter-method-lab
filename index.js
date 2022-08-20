639dbf9c8165d5f566fe7dccd0192ce6127579ad
Josekariz <joseph.macharia@student.moringaschool.com>
2022-08-21 01:26:13 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(drivers, str) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === str.toLowerCase();
+  });
+}
+// console.log(findMatching(drivers, "Bobby")
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(function (getDriver) {
+    return (
+      getDriver.toLowerCase().substring(0, str.length) === str.toLowerCase()
+    );
+  });
+}
+// console.log(fuzzyMatch(drivers, "a"));
+
+// const drivers = [
+//   {
+//     name: "Bobby",
+//     hometown: "Pittsburgh",
+//   },
+//   {
+//     name: "Sammy",
+//     hometown: "New York",
+//   },
+//   {
+//     name: "Sally",
+//     hometown: "Cleveland",
+//   },
+//   {
+//     name: "Annette",
+//     hometown: "Los Angeles",
+//   },
+//   {
+//     name: "Bobby",
+//     hometown: "Tampa Bay",
+//   },
+// ];
+
+function matchName(drivers, str) {
+  return drivers.filter(function (driverProp) {
+    return driverProp.name === str;
+  });
+}
+//console.log(matchName(drivers, "Bobby"));
